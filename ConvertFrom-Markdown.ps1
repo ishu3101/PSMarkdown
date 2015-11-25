aec0aa2f7472c96860e26cf3fcdd02e7b81cea52
ishu3101 <ishu3101@hotmail.com>
2015-11-25 15:53:46 +1300

    Added support for accepting data from pipeline for the ConvertFrom-Markdown function

--- ConvertFrom-Markdown.ps1
+++ ConvertFrom-Markdown.ps1
@@ -1,8 +1,28 @@
 # Convert From Markdown Table to PowerShell Object
-function ConvertFrom-Markdown($InputObject){
-    $myobject = $InputObject
-    $mddata = $myobject
-    $data = $mddata | Where-Object {$_ -notmatch "--" }
-    $object = $data -replace ' +', ''| ConvertFrom-Csv -Delimiter '|'
-    $object
+Function ConvertFrom-Markdown {
+    [CmdletBinding()]
+    [OutputType([string])]
+    Param (
+        [Parameter(
+            Mandatory = $true,
+            Position = 0,
+            ValueFromPipeline = $true
+        )]
+        $InputObject
+    )
+
+    Begin {
+        $items = @()
+    }
+
+    Process {
+        $mddata = $InputObject
+        $data = $mddata | Where-Object {$_ -notmatch "--" }
+        $items += $data
+    }
+
+    End {
+       $object = $items -replace ' +', '' | ConvertFrom-Csv -Delimiter '|'
+       $object 
+    }
 }
