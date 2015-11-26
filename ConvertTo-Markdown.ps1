d6041a3760deb213e54fb8c36506b1872cc5f72a
ishu3101 <ishu3101@hotmail.com>
2015-11-27 09:45:45 +1300

    Update ConvertTo-Markdown function help with description and examples

--- ConvertTo-Markdown.ps1
+++ ConvertTo-Markdown.ps1
@@ -1,10 +1,20 @@
 ﻿<#
 .Synopsis
    Converts a PowerShell object to a Markdown table.
+.Description
+   The ConvertTo-Markdown function converts a Powershell Object to a Markdown formatted table
 .EXAMPLE
-   $data | ConvertTo-Markdown
+   Get-Process | Where-Object {$_.mainWindowTitle} | Select-Object ID, Name, Path, Company | ConvertTo-Markdown
+
+   This command gets all the processes that have a main window title, and it displays them in a Markdown table format with the process ID, Name, Path and Company.
 .EXAMPLE
-   ConvertTo-Markdown($data)
+   ConvertTo-Markdown (Get-Date)
+
+   This command converts a date object to Markdown table format
+.EXAMPLE
+   Get-Alias | Select Name, DisplayName | ConvertTo-Markdown
+
+   This command displays the name and displayname of all the aliases for the current session in Markdown table format
 #>
 Function ConvertTo-Markdown {
     [CmdletBinding()]
